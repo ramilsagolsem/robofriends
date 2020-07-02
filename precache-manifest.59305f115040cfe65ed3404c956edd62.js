b25e18f5f53e7e7115cbc45beab29d90fddcf4f3
ramilsagolsem <ramilsagolsem@gmail.com>
2020-07-01 22:52:00 -0300

    Updates

---
+++ precache-manifest.59305f115040cfe65ed3404c956edd62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261387f560a103adddd2f3182d08268e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "deb4645bae63f625f0ec",
+    "url": "/robofriends/static/css/main.21a05347.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb4645bae63f625f0ec",
+    "url": "/robofriends/static/js/main.86f20872.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
